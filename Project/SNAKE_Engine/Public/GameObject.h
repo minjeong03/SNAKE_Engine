bf209824cde258a71b89b9b5079b87715f86848d
Jinwoo Choi <76608214+Nero-TheThrill@users.noreply.github.com>
2025-07-30 12:28:50 +0900

    now user can use gameobject without inherit & create another class

--- Project/SNAKE_Engine/Public/GameObject.h
+++ Project/SNAKE_Engine/Public/GameObject.h
@@ -7,4 +7,15 @@ class GameObject : public Object
 public:
     GameObject() : Object(ObjectType::GAME) {}
     ~GameObject() override = default;
+    void Init([[maybe_unused]] const EngineContext& engineContext) override {}
+
+    void LateInit([[maybe_unused]] const EngineContext& engineContext) override {}
+
+    void Update([[maybe_unused]] float dt, [[maybe_unused]] const EngineContext& engineContext) override {}
+
+    void Draw([[maybe_unused]] const EngineContext& engineContext) override {}
+
+    void Free([[maybe_unused]] const EngineContext& engineContext) override {}
+
+    void LateFree([[maybe_unused]] const EngineContext& engineContext) override {}
 };
